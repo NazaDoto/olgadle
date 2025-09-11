72064f3fe6ac74d4e86f0c7b4264023bb71f28a8
Naza <nazadoto@gmail.com>
2025-09-11 11:32:52 -0300

    pelao si canta

--- src/Olgadle.vue
+++ src/Olgadle.vue
@@ -55,7 +55,7 @@
       <div v-else class="c-white text-center mb-2">
         <h2 :class="(terminado == -1) ? 'texto-perdiste' : 'texto-ganaste'"> {{ (terminado == -1) ? 'Perdiste' :
           '¡Ganaste!'
-          }}</h2>
+        }}</h2>
         <p class="c-white">{{ 'Acertaron ' + aciertos + ' de ' + intentosTotales + ' personas.' }}</p>
         <span v-if="terminado != -1">
 
@@ -563,7 +563,7 @@ export default {
           genero: "Masculino",
           programa: ["TDL", "Faltan Varones"],
           rol: "Invitado / Conductor",
-          canta: "No",
+          canta: "Si",
           hizo: "IG/TikTok",
           nacio: "1997",
         },
