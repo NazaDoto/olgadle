c262dec23386919211879cd1108b9d7c7e18b28f
Naza <nazadoto@gmail.com>
2026-04-16 08:37:56 -0300

    a ver

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import axios from 'axios'
 
 // En dev apunta al backend local, en prod las rutas son relativas (nginx las proxea)
-axios.defaults.baseURL = import.meta.env.DEV ? 'http://localhost:3501' : ''
+axios.defaults.baseURL = import.meta.env.DEV ? 'http://localhost:3501' : 'api'
 
 const app = createApp(App)
 app.use(router)
